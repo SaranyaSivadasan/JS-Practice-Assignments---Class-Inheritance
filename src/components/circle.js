805879137c009f72de069c6a6ae10d900df487ba
SaranyaSivadasan <68867201+SaranyaSivadasan@users.noreply.github.com>
2020-12-04 13:53:30 +0530

    change

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,17 @@
 
 // declare class
 
-
 // export class using module.exports
+
+let Shape = require("./shape");
+class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "blue";
+  }
+  calculateArea() {
+    console.log("calculate");
+  }
+}
+
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,21 @@
 // declare class
 
 // export class using module.exports
+
+import { Component } from "react";
+
+class Shape extends Component {
+  constructor(color) {
+    super();
+    this.color = "red";
+  }
+  drawShape() {
+    console.log("draw a shape");
+  }
+
+  calculateArea() {
+    console.log("calculate the area ");
+  }
+}
+
+module.exports = Shape;
